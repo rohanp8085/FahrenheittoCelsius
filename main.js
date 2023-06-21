5a02043b002d3c643c2b47c7609466a9ba6bf1ba
unknown <rohanprajapati3739@gmail.com>
2023-06-21 14:19:48 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const cel = document.querySelector("#cel")
+const fah = document.querySelector("#fah")
+
+
+cel.addEventListener("input" , function () {
+    let c = this.value
+    let f = (c * 9/5) + 32
+    fah.value = f
+})
+
+fah.addEventListener("input" , function () {
+    let f = this.value
+    let c = (f - 32) * 5/9 
+    cel.value = c
+})
